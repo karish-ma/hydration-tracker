f28840422abcd4c1614f63c234c35fe7c5b61ba4
Karishma M <karishma.mhapadi@gmail.com>
2026-06-15 17:35:15 -0500

    fix Modal app variable name to 'app' as required by Modal CLI

--- modal_app.py
+++ modal_app.py
@@ -1,7 +1,7 @@
 import modal
 from main import app as flask_app, send_daily_summaries, send_nudges
 
-modal_app = modal.App("hydration-tracker")
+app = modal.App("hydration-tracker")
 
 image = (
     modal.Image.debian_slim(python_version="3.12")
@@ -11,17 +11,17 @@ image = (
 secrets = [modal.Secret.from_name("hydration-tracker-secrets")]
 
 
-@modal_app.function(image=image, secrets=secrets, schedule=modal.Cron("30 14 * * *"))
+@app.function(image=image, secrets=secrets, schedule=modal.Cron("30 14 * * *"))
 def run_daily_summaries():
     send_daily_summaries()
 
 
-@modal_app.function(image=image, secrets=secrets, schedule=modal.Cron("30 7 * * *"))
+@app.function(image=image, secrets=secrets, schedule=modal.Cron("30 7 * * *"))
 def run_nudges():
     send_nudges()
 
 
-@modal_app.function(image=image, secrets=secrets)
+@app.function(image=image, secrets=secrets)
 @modal.wsgi_app()
 def web():
     return flask_app
